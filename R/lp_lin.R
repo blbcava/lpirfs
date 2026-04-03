a49fd769625429380ec8fb2a7f38a049d291730c
Bruno Cavalcante <111328205+blbcava@users.noreply.github.com>
2026-04-03 17:36:54 -0300

    Fix typo in parameter documentation for adjust_se

--- R/lp_lin.R
+++ R/lp_lin.R
@@ -15,7 +15,7 @@
 #' @param nw_lag Integer. Specifies the maximum lag with positive weight for the Newey-West estimator. If set to NULL (default), the lag increases with
 #'               with the number of horizon.
 #' @param nw_prewhite Boolean. Should the estimators be pre-whitened? TRUE or FALSE (default).
-#' @param adjust_se Boolen. Should a finite sample adjsutment be made to the covariance matrix estimators? TRUE or FALSE (default).
+#' @param adjust_se Boolean. Should a finite sample adjsutment be made to the covariance matrix estimators? TRUE or FALSE (default).
 #' @param hor Integer. Number of horizons for impulse responses.
 #'@param exog_data A \link{data.frame}, containing exogenous variables for the VAR. The row length has to be the same as \emph{endog_data}.
 #'                  Lag lengths for exogenous variables have to be given and will not be determined via a lag length criterion.
